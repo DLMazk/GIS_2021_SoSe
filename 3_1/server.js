d1f2dcf1d4b00e586cdf6ef6aaf0edd3cf3e7369
Daniel <daniel.litterst@web.de>
2021-05-28 12:39:42 +0200

    commit 3_1

--- 3_1/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Aufgabe3_1 = void 0;
-const Http = require("http");
-var Aufgabe3_1;
-(function (Aufgabe3_1) {
-    Http.createServer(...);
-})(Aufgabe3_1 = exports.Aufgabe3_1 || (exports.Aufgabe3_1 = {}));
-//# sourceMappingURL=server.js.map
--- 3_1/server.ts
+++
@@ -1,9 +0,0 @@
-import * as Http from "http";
-import * as Url from "url";
-
-export namespace Aufgabe3_1 {
-    
-    Http.createServer(...);
-
-
-}

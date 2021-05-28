87c85a2fd8689c01e1b1eafa78cd0bcfd9991223
Daniel <daniel.litterst@web.de>
2021-05-28 13:33:29 +0200

    commit Test

---
+++ 3_1/test.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("hallo Node JS Server");
+test();
+function test() {
+    let x = 10;
+    let a = ["hallo", "gh", "sdf"];
+    console.log(a[1], x);
+}
+//# sourceMappingURL=test.js.map
---
+++ 3_1/test.ts
@@ -0,0 +1,9 @@
+console.log("hallo Node JS Server");
+test();
+
+function test(): void {
+    let x: number = 10;
+    let a: string[] = ["hallo", "gh", "sdf"];
+
+    console.log(a[1], x);
+}

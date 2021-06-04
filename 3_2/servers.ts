9eee767bc2fdac2f0d9a857c0428e2c017ee846c
DLMazk <mclittle17@gmail.com>
2021-06-04 10:27:24 +0200

    bissle rumprobieren

--- 3_2/servers.ts
+++ 3_2/servers.ts
@@ -2,35 +2,35 @@ import * as Http from "http";
 import * as url from "url";
 
 export namespace Aufgabe3_1 {
-    // console.log("Starting server"); //Konsolenausgabe
-    // let port: number = Number(process.env.PORT); //Port ist "Hafen" 
-    // if (!port)
-    //     port = 8100; //Prot wird auf 8100 gesetzt (localhost:8100)
+    console.log("Starting server"); //Konsolenausgabe
+    let port: number = Number(process.env.PORT); //Port ist "Hafen" 
+    if (!port)
+        port = 8100; //Prot wird auf 8100 gesetzt (localhost:8100)
 
-    // let server: Http.Server = Http.createServer(); //erstellen eines einfachen Servers
-    // server.addListener("request", handleRequest);
-    // server.addListener("listening", handleListen);
-    // server.listen(port);
+    let server: Http.Server = Http.createServer(); //erstellen eines einfachen Servers
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
 
-    // function handleListen(): void {
-    //     console.log("Listening"); //Konsolenausgabe
-    // }
+    function handleListen(): void {
+        console.log("Listening"); //Konsolenausgabe
+    }
 
 
-    // function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-    //     console.log("I hear voices!"); //Konsolenausgabe 
-    //     console.log(_request.url); //Konsolenausgabe 
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!"); //Konsolenausgabe 
+        console.log(_request.url); //Konsolenausgabe 
 
-    //     _response.setHeader("content-type", "text/html; charset=utf-8"); //Eigenschaften; Typ:Html
-    //     _response.setHeader("Access-Control-Allow-Origin", "*"); //Zugriffserlaubnis --> * alle dürfen zugreifen 
-    //     _response.write(_request.url); //was wird zurückgegeben: Als Antwort auf die Anfrage 
-    //     _response.end(); //Antwort fertig und zurückschicken 
-    // }
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Eigenschaften; Typ:Html
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Zugriffserlaubnis --> * alle dürfen zugreifen 
+        _response.write(_request.url); //was wird zurückgegeben: Als Antwort auf die Anfrage 
+        _response.end(); //Antwort fertig und zurückschicken 
+    }
 
 
     //Diesen Code innerhalb von einem aktiven Server testen:
 
-    let adresse: string = 'http://localhost:5500/default.htm?jahr=2017&monat=february';
+    let adresse: string = 'http://localhost:8100/default.htm?jahr=2017&monat=february';
     //Adresse parsen (umwandeln):
     let q = url.parse(adresse, true);
 

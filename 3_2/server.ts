321868f09cce5d9f8f2b58b669f8470da6861f38
Daniel <daniel.litterst@web.de>
2021-06-06 17:20:28 +0200

    It's a wrap

--- 3_2/server.ts
+++ 3_2/server.ts
@@ -3,42 +3,42 @@ import * as Http from "http";
 import * as Url from "url"; 
 
 export namespace Aufgabe3_2 {
-    console.log("Starting server"); //Konsolenausgabe
-    let port: number = Number(process.env.PORT); //Port ist "Hafen" 
+    console.log("Starting server"); 
+    let port: number = Number(process.env.PORT); 
     if (!port)
-        port = 8100; //Prot wird auf 8100 gesetzt (localhost:8100)
+        port = 8100; 
 
-    let server: Http.Server = Http.createServer(); //erstellen eines einfachen Servers
+    let server: Http.Server = Http.createServer(); 
     server.addListener("request", handleRequest); 
     server.addListener("listening", handleListen);
     server.listen(port);
 
     function handleListen(): void {
-        console.log("Listening"); //Konsolenausgabe
+        console.log("Listening"); 
     }
 
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!"); //Konsolenausgabe 
-        console.log(_request.url); //gibt Url-Bounce zurück --> soll jetzt geändert werden
+        console.log("I hear voices!"); 
+        console.log(_request.url); 
         
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Eigenschaften; Typ:Html
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Zugriffserlaubnis --> * alle dürfen zugreifen 
+        _response.setHeader("content-type", "text/html; charset=utf-8"); 
+        _response.setHeader("Access-Control-Allow-Origin", "*"); 
         
         if (_request.url) {
-            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true); //umwandeln in assoziatives Array, um Daten später rauszulesen 
-            let pfad: string = <string>url.pathname; //pathname der Url in String speichern
-            if (pfad == "/html") { //um hier Überprüdung machn zu können, ob welchen path man gegenagen ist 
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true); 
+            let pfad: string = <string>url.pathname; 
+            if (pfad == "/html") { 
                 for (let key in url.query) {
-                    _response.write(key + ":" + url.query [key] + "<br/>"); 
+                    _response.write(key + ": " + url.query [key] + "<br/>"); 
                 }  
             }
-            else if (pfad == "/json") { //um hier Überprüdung machn zu können, welchen path man gegangen ist 
+            else if (pfad == "/json") { 
                 let jsonString: string = JSON.stringify(url.query); 
-                console.log(jsonString); //in Konsole
-                _response.write(jsonString); //Anwort, die zurückkommt   
+                console.log(jsonString); 
+                _response.write(jsonString);  
             }
         }
-        _response.end(); //Antwort fertig und zurückschicken 
+        _response.end(); 
     }
 }
